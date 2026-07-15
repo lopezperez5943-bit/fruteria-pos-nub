d95fe75cd74e9e4304f1e35b8313fabba2d94295
imbirt <juanlopezperez5943@gmail.com>
2026-07-14 21:13:33 -0600

    Update app_visual.py

--- app_visual.py
+++ app_visual.py
@@ -2,7 +2,7 @@ import streamlit as st
 import requests
 import urllib.parse
 
-API_URL = "http://127.0.0.1:8000"
+API_URL = "https://fruteria-pos-nub.onrender.com"
 
 st.set_page_config(page_title="Frutería POS & Inventario", page_icon="🍏", layout="wide")
 
